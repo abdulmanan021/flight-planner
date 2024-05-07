6b3b752e5abdece99226acf6c0bf64e84462a684
Abdul Manan <abdul.manan@tecishsol.com>
2024-05-07 17:27:48 +0500

    vercel setting

--- app.js
+++ app.js
@@ -8,11 +8,7 @@ const Flight = require("./db/Flight")
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: "https://flight-planner-eta.vercel.app/",
-    methods: ["POST", "GET", "DELETE", "PUT"],
-    credentials: true
-}));
+app.use(cors());
 
 app.post("/signup", async (req, resp) => {
     let user = new User(req.body);
